db24cfa7049f2246813bb4185d31deae0655343a
Eric Na <eric.na@qualia.com>
2019-07-29 20:23:09 -0700

    RateFormPage; ParserInput

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
-import { Button } from 'semantic-ui-react';
+import {
+  Button, Input, Label, Form, TextArea, Dropdown, Header, Segment, Radio
+} from 'semantic-ui-react'
+
 import axios from 'axios';
+import _ from 'underscore';
 
 import AppContext from './contexts/AppContext';
 
@@ -12,13 +16,170 @@ import 'semantic-ui-css/semantic.min.css'
 import './App.css';
 
 function App() {
-  const [stateCode, setStateCode] = useState('');
-  const [underwriter, setUnderwriter] = useState('d');
-  const [promulgatedStates, setPromulgatedStates] = useState([]);
+  const [stateCode, setStateCode] = useState('CA');
+  const [underwriter, setUnderwriter] = useState('WFG');
   const resetStates = () => {
     setStateCode('');
     setUnderwriter('');
   }
+
+
+  return (
+    <AppContext.Provider value={{
+        stateCode,
+        setStateCode,
+        underwriter,
+        setUnderwriter,
+      }}>
+      <div className="App">
+      <Container>
+        <div>
+          <UnderwritersDropdown onChange={(e, data) => setUnderwriter(data.value)}/>
+          <StatesDropdown onChange={(e, data) => setStateCode(data.value)}/>
+          <Button onClick={() => {resetStates()}}>Reset</Button>
+        </div>
+        {
+          (stateCode && underwriter) &&
+          <RateFormPage />
+        }
+      </Container>
+      </div>
+    </AppContext.Provider>
+  );
+}
+
+function ParserInput() {
+  const [tableType, setTableType] = useState('');
+
+  const [tableText, setTableText] = useState('');
+  const [numCols, setNumCols] = useState(2);
+  const [toAmountColIndex, setToAmountColIndex] = useState(1);
+  const [premiumColIndex, setPremiumColIndex] = useState(2);
+
+  const [additionalTableText, setAdditionalTableText] = useState('');
+  const [additionalNumCols, setAdditionalNumCols] = useState(2);
+  const [additionalToAmountColIndex, setAdditionalToAmountColIndex] = useState(1);
+  const [additionalPremiumColIndex, setAdditionalPremiumColIndex] = useState(2);
+
+  const TableTypeRadioButtons = ({tableType, setTableType}) => {
+    const tableTypes = ['Fixed', 'Graduated', 'Joint'];
+
+    return (
+      <div>
+        {
+          tableTypes.map(val => (
+            <Form.Field key={`tableTypeRadioButton-${val}`}>
+              <Radio
+                label={val}
+                name='radioGroup'
+                value={val}
+                checked={tableType === val}
+                onChange={() => {setTableType(val)}}
+              />
+            </Form.Field>
+          ))
+        }
+      </div>
+    );
+  };
+
+  const TableTextInput = (
+    {tableType, numCols, toAmountColIndex, premiumColIndex,
+      setNumCols, setToAmountColIndex, setPremiumColIndex, tableText, setTableText
+    }) => {
+      const numberOptions = _.range(1, 9).map(x => ({key: x, value: x, text: x}));
+
+      return (
+        <div>
+          <Form.Field>
+            <label>{tableType} Table Text</label>
+            <Form.TextArea
+              placeholder='Money Money'
+              value={tableText}
+              onChange={(e, data) => {e.preventDefault(); setTableText(data.value)}}
+            />
+          </Form.Field>
+          <label>Number of Columns with $</label>
+          <Dropdown
+            placeholder='Number of Columns with $'
+            floating
+            options={numberOptions}
+            defaultValue={numCols}
+            onChange={(e, data) => {setNumCols(data.value)}}
+          />
+          <label>'To Amount' Column Index</label>
+          <Dropdown
+            placeholder={'Index of the Column for \'To Amount\' (Upper Bound)'}
+            floating
+            options={_.take(numberOptions, numCols)}
+            defaultValue={toAmountColIndex}
+            onChange={(e, data) => {setToAmountColIndex(data.value)}}
+          />
+          <label>Premium Column Index</label>
+          <Dropdown
+            placeholder='Index of the Column for Insurance Premium'
+            floating
+            options={_.take(numberOptions, numCols)}
+            defaultValue={premiumColIndex}
+            onChange={(e, data) => {setPremiumColIndex(data.value)}}
+          />
+        </div>
+      );
+  };
+
+  const isJointTable = tableType === 'Joint';
+
+  return (
+      <Segment raised>
+        <Header size='medium'>Add {tableType} Table</Header>
+
+        <Form.Field>
+          <TableTypeRadioButtons
+            tableType={tableType}
+            setTableType={setTableType}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Table Name</label>
+          <Input labelPosition='right' type='text' placeholder='Amount' onChange={(e) => {}}>
+            <input placeholder='Name of the table' />
+          </Input>
+        </Form.Field>
+
+        <TableTextInput
+          tableType={!isJointTable ? tableType : 'Fixed'}
+          tableText={tableText}
+          numCols={numCols}
+          toAmountColIndex={toAmountColIndex}
+          premiumColIndex={premiumColIndex}
+          setTableText={setTableText}
+          setNumCols={setNumCols}
+          setToAmountColIndex={setToAmountColIndex}
+          setPremiumColIndex={setPremiumColIndex}
+        />
+        {
+          isJointTable && (
+            <TableTextInput
+              tableType='Graduated'
+              tableText={additionalTableText}
+              numCols={additionalNumCols}
+              toAmountColIndex={additionalToAmountColIndex}
+              premiumColIndex={additionalPremiumColIndex}
+              setTableText={setAdditionalTableText}
+              setNumCols={setAdditionalNumCols}
+              setToAmountColIndex={setAdditionalToAmountColIndex}
+              setPremiumColIndex={setAdditionalPremiumColIndex}
+            />
+          )
+        }
+      </Segment>
+  );
+}
+
+
+function RateFormPage() {
+  const [promulgatedStates, setPromulgatedStates] = useState([]);
+  const [cplFee, setCplFee] = useState();
 
   useEffect(() => {
     // We moved these functions inside!
@@ -35,45 +196,47 @@ function App() {
     fetchData();
   }, []);
 
+
+  const handleSubmit = (payload) => {
+    axios.post(`http://localhost:4567/generate_rate_files`, payload).then(res => {
+      console.log(res);
+      console.log(res.data);
+    });
+  }
+
   return (
-    <AppContext.Provider value={{
-        stateCode,
-        setStateCode,
-        underwriter,
-        setUnderwriter,
-      }}>
-      <div className="App">
-        <AppContext.Consumer>
-          {context => (
-            !(stateCode && underwriter) ? (
-              <div>
-                <UnderwritersDropdown onChange={(e, data) => setUnderwriter(data.value)}/>
-                <StatesDropdown onChange={(e, data) => setStateCode(data.value)}/>
-              </div>
-            ) : (
-              <div>
-                <Button onClick={() => {resetStates()}}>Go Back</Button>
-                <Container>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-                    Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur
-                    ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
-                    consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
-                    In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
-                    link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean
-                    vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac,
-                    enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla
-                    ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue.
-                    Curabitur ullamcorper ultricies nisi.
-                  </p>
-                </Container>
-                {promulgatedStates.includes(stateCode) && <Button onClick={() => {}}>Generate Rates</Button>}
-              </div>
-            )
-          )}
-        </AppContext.Consumer>
-      </div>
-    </AppContext.Provider>
+    <AppContext.Consumer>
+    { context => {
+      const formPayload = {stateCode: context.stateCode, underwriter: context.underwriter};
+      if (promulgatedStates.includes(context.stateCode)) {
+        return <Button onClick={() => {handleSubmit(formPayload)}}>Submit</Button>;
+      }
+
+      return (
+        <div>
+          <Form>
+            <Form.Field>
+            <label>CPL Fee</label>
+            <Input labelPosition='right' type='text' placeholder='Amount' onChange={(e) => {setCplFee(e.target.value);}}>
+              <Label basic>$</Label>
+              <input placeholder='CPL Fee' />
+            </Input>
+            </Form.Field>
+
+            <ParserInput />
+
+            <Button
+              type="submit"
+              onClick={(e, value) => {
+                Object.assign(formPayload, {cpl_fee: cplFee});
+                handleSubmit(formPayload);
+              }}>Submit</Button>
+          </Form>
+        </div>
+      );
+      }
+    }
+    </AppContext.Consumer>
   );
 }
 
